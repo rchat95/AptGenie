671bb2923f3fdd459f739b78655e8a9c64c1089a
Rongon Chatterjee <rongon95@gmail.com>
2020-01-23 10:39:25 -0600

    Initial MVVM Setup. Created SignInPageVM

---
+++ AptGenie/AptGenie/ViewModels/SignInPageVM.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace AptGenie.ViewModels
+{
+    public class SignInPageVM
+    {
+        public SignInPageVM()
+        {
+            TestCommand = new Command(() =>
+            {
+                Application.Current.MainPage.DisplayAlert("Alert", "Button clicked", "OK");
+            });
+        }
+
+        public Command TestCommand { get; }
+    }
+}
